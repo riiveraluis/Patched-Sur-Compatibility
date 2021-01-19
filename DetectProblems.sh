f2540bf8b26fdc8b101b2a62f47e07ead2bb5aa6
Ben Sova <65474198+BenSova@users.noreply.github.com>
2021-01-18 22:34:28 -0700

    Oh wait, I forgot to actually make the changes that make DetectProblems.sh bad.

--- DetectProblems.sh
+++ DetectProblems.sh
@@ -40,7 +40,7 @@ print
 print "Starting detections..."
 
 print "Checking for FileVault..."
-if [[ "$(fdesetup status)" = "FileVault is On." ]]
+if [[ "$(fdesetup status)" = "FileVault is Off." ]]
 then
     FILEVAULT="Fatal;FileVault is Enabled;FileVault prevents use of recovery mode, resulting in a always incorrect password when trying to unlock recovery. You must disable FileVault before continuing."
     FVHUMAN="Fatal: FileVault is Enabled. FileVault prevents use of recovery mode, resulting in a always incorrect password when trying to unlock recovery.You must disable FileVault before continuing."
